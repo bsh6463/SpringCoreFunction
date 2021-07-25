57c00360d7a09998277aef1f196c7dc8d6614aa5
bsh6463 <bsh9510@gmail.com>
2021-07-25 22:47:17 +0900

    210725, Spring Container

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,24 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+       // AppConfig appConfig = new AppConfig();
 
-        MemberService memberService = appConfig.memberService();
+        //MemberService memberService = appConfig.memberService();
      //   MemberService memberService = new MemberServiceImpl();
+
+        //spring container, appConfig의 설정 정보를 가지고 bean으로 등록하여 관리함.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //Appconfig에서 찾아오는게 아니라 applicationContext에서 찾아옴.
+        //컨테이너에는 @Bean붙인 메서드의 이름으로 등록됨
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "A", Grade.VIP);
         memberService.join(member);
