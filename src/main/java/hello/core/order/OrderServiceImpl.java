831fe8c2aec3f3f62a14e8865d7fdb149da71229
bsh6463 <bsh9510@gmail.com>
2021-07-30 15:46:02 +0900

    210730 @Qualifier + annotaion

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotaion.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-@RequiredArgsConstructor //final 붙은 필드를 받는 생성자를 만듦
+//@RequiredArgsConstructor //final 붙은 필드를 받는 생성자를 만듦
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
@@ -19,11 +20,11 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy; //이렇게하면 인터페이스에만 의존함.
 //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
